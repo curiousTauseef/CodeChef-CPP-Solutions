832eb335f3747841a0cfb0a809ddf85dae69dad8
ktejas <krishnasalimeda@gmail.com>
2015-11-13 22:55:02 -0700

    Used Bubble Sort but crossed Time Limit

--- 006_TurboSort/main.cpp
+++ 006_TurboSort/main.cpp
@@ -1,3 +1,6 @@
+//Bubble Sort
+//http://www.cprogramming.com/tutorial/computersciencetheory/sortcomp.html
+
 #include<iostream>
 
 using namespace std;
@@ -15,6 +18,23 @@ int main()
 		cin >> n[i];
 	}
 
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 0; j < t-1; j++)
+		{
+			if (n[j + 1] > n[j])
+			{
+				int temp = n[j + 1];
+				n[j + 1] = n[j];
+				n[j] = temp;
+			}
+		}
+	}
 
+	for (int i = 0; i < t; i++)
+	{
+		cout << n[i] << endl;
+	}
 
+	system("PAUSE");
 }
